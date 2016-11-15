53f14044ca48c34efaffd30506dffca66aac1a00
Manu Uzkudun <uzkudun@mdpi.com>
2016-11-15 17:57:49 +0100

    es6 transpile with babel configured in webpack

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-require("./less/style.less");
-require("jquery");
-require("animate.css");
-require("../node_modules/jquery-waypoints/waypoints.min.js");
+import "./less/style.less";
+import "animate.css";
+import "../node_modules/jquery-waypoints/waypoints.min.js";
+import $ from 'jquery';
+import jQuery from 'jquery';
+
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
+
+$( document ).ready ( () => {
+  console.log('readyyyy!!');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,27 @@ module.exports = {
   module: {
     loaders: [
       {
+        // create working environment for es6
+        // need to npm i babel-loader babel-core babel-preset-es2015 -D
+        // https://github.com/babel/babel-loader
+        test:/\.js$/,
+        exclude:'/node_modules',
+        loader:'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
         test: /\.(otf|jpg|png)$/,
         loader: 'url?limit=25000'
       },
       {
-        test: /\.less$/,
-        loader: "style!css!less"
+        // take less convert to css and inject to style tag
+        // need to: npm i css-loader less-loader less  style-loader -D
+        // https://github.com/webpack/less-loader
+        test:/\.less$/,
+        exclude:'/node_modules',
+        loader:"style!css!less"
       },
       {
         test: /\.css$/,
