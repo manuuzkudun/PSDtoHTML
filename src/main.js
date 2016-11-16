fb913dc6b897eec93652c6fdd71418bb5f344de0
Manu Uzkudun <uzkudun@mdpi.com>
2016-11-16 11:54:57 +0100

    Iphone animation working

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 import "./less/style.less";
 import "animate.css";
-import "../node_modules/jquery-waypoints/waypoints.min.js";
-import $ from 'jquery';
-import jQuery from 'jquery';
+import $ from "jquery";
+import jQuery from "jquery";
+require ("../node_modules/waypoints/lib/noframework.waypoints.min.js");
 
 // export for others scripts to use
 window.$ = $;
 window.jQuery = jQuery;
 
 $( document ).ready ( () => {
-  console.log('readyyyy!!');
+
+  const iphoneWp = document.querySelector('.iphone-js-wp');
+  const iphoneOn = document.querySelector('.iphone-js-wp > .iphone-on');
+  const iphoneBtn = document.querySelector('.iphone-js-wp > .iphone-btn');
+
+  const waypoint = new Waypoint({
+    element: iphoneWp,
+    handler: direction => {
+      $(iphoneWp).addClass('animated slideInUp');
+      $(iphoneOn).addClass('animated fadeIn');
+    },
+    offset: "70%"
+  });
+
+  // Animation of pushing and then releasing the iphone button
+  $(iphoneBtn).delay(2300).animate({bottom: "+=-3"}, 300);
+  $(iphoneBtn).delay(300).animate({bottom: "+=3"}, 100);
+
 });
